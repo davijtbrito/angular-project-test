365a5607441f7bd6befe29236257029554ea212b
davibrito1218 <davibrito1218@gmail.com>
2023-05-26 10:39:08 +0100

    Hoem and About page with Route implementation

--- angular-project-test/src/app/app-routing.module.ts
+++ angular-project-test/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },  // Home page route
+  { path: 'about', component: AboutComponent },  // About page route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
